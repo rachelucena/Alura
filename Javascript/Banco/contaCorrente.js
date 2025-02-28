c3c6363844a3e569d6b53b2cd37b2e7c88397c9e
rachelucena <159203793+rachelucena@users.noreply.github.com>
2025-02-28 10:08:42 -0500

    JavaScript: programando a Orientação a Objetos
    Aulas 4 e 5

--- Javascript/Banco/contaCorrente.js
+++ Javascript/Banco/contaCorrente.js
@@ -1,7 +1,30 @@
 export class ContaCorrente {
+    static numeroDeContas = 0;
     _saldo = 0;
     agencia;
     cliente;
+
+
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    constructor(agencia, cliente) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
--- Javascript/Banco/index.js
+++ Javascript/Banco/index.js
@@ -1,17 +1,13 @@
-import { Cliente } from "./cliente";
-import { ContaCorrente } from "./contaCorrente";
+import { Cliente } from "./cliente.js";
+import { ContaCorrente } from "./contaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = 'Ricardo';
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente('Ricardo', 11122233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = 'Alice';
-cliente2.cpf = 88822233309;
+const cliente2 = new Cliente('Alice', 88822233309);
 
-const contaCorrenteRicardo = new ContaCorrente()
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+let numeroDeContas = 0;
+
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 
 contaCorrenteRicardo.depositar(100); // 100 reais de deposito
 contaCorrenteRicardo.depositar(200);
@@ -20,13 +16,12 @@ contaCorrenteRicardo.depositar(-1);
 const valorSacado = contaCorrenteRicardo.sacar(50); // valor que vai sacar
 console.log(valorSacado);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 1002;
+const conta2 = new ContaCorrente(1002, cliente2);
 
 contaCorrenteRicardo.transferir(200, conta2);
 console.log(conta2);
 
 
 console.log(cliente1);
-console.log(cliente2)
+console.log(cliente2);
+console.log(ContaCorrente.numeroDeContas);
