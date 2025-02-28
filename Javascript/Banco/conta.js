91e57e26a8f56303256a3e4f78246820fe08a143
rachelucena <159203793+rachelucena@users.noreply.github.com>
2025-02-28 11:08:46 -0500

    JavaScript: interfaces e Herança em Orientação a Objetos
    Aula 1

--- Javascript/Banco/conta.js
+++ Javascript/Banco/conta.js
@@ -1,27 +1,8 @@
-import { Cliente } from "./cliente.js";
-
-export class ContaCorrente {
-    static numeroDeContas = 0;
-   
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
-    }
-
-    get cliente() {
-        return this._cliente;
-    }
-
-    get saldo() {
-        return this._saldo;
-    }
-
-    constructor(agencia, cliente) {
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
-        ContaCorrente.numeroDeContas += 1;
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
     }
 
     sacar(valor) {
--- Javascript/Banco/index.js
+++ Javascript/Banco/index.js
@@ -1,27 +1,16 @@
 import { Cliente } from "./cliente.js";
 import { ContaCorrente } from "./contaCorrente.js";
+import { ContaPoupanca } from "./contaPoupanca.js";
+import { Conta } from "./conta.js";
 
 const cliente1 = new Cliente('Ricardo', 11122233309);
 
-const cliente2 = new Cliente('Alice', 88822233309);
+const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
 
-let numeroDeContas = 0;
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
-contaCorrenteRicardo.depositar(100); // 100 reais de deposito
-contaCorrenteRicardo.depositar(200);
-contaCorrenteRicardo.depositar(-1);
-
-const valorSacado = contaCorrenteRicardo.sacar(50); // valor que vai sacar
-console.log(valorSacado);
-
-const conta2 = new ContaCorrente(1002, cliente2);
-
-contaCorrenteRicardo.transferir(200, conta2);
-console.log(conta2);
-
-
-console.log(cliente1);
-console.log(cliente2);
-console.log(ContaCorrente.numeroDeContas);
+console.log(contaCorrenteRicardo);
+console.log(contaPoupanca);
