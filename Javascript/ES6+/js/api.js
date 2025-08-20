786b03863ac8dd02c46d6695021353e651b7bc5d
rachelucena <159203793+rachelucena@users.noreply.github.com>
2025-08-20 09:20:36 -0400

    Curso ES6+
    Aulas finais.

--- Javascript/ES6+/js/api.js
+++ Javascript/ES6+/js/api.js
@@ -1,10 +1,22 @@
 const URL_BASE = "http://localhost:3000"
 
+const converterStringParaData = (dataString) => {
+  const [ano, mes, dia] = dataString.split("-")
+  return new Date(Date.UTC(ano, mes - 1, dia))
+}
+
 const api = {
   async buscarPensamentos() {
     try {
       const response = await axios.get(`${URL_BASE}/pensamentos`)
-      return await response.data
+      const pensamentos = await response.data
+
+      return pensamentos.map(pensamento => {
+        return {
+          ...pensamento,
+          data: new Date(pensamento.data)
+        }
+      })
     }
     catch {
       alert('Erro ao buscar pensamentos')
@@ -14,7 +26,12 @@ const api = {
 
   async salvarPensamento(pensamento) {
     try {
-      const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento)
+      const data = converterStringParaData(pensamento.data)
+
+      const response = await axios.post(`${URL_BASE}/pensamentos`, {
+        ...pensamento,
+        data: data.toISOString()
+      })
       return await response.data
     }
     catch {
@@ -26,7 +43,12 @@ const api = {
   async buscarPensamentoPorId(id) {
     try {
       const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
-      return await response.data
+      const pensamento = await response.data
+
+      return {
+        ...pensamento,
+        data: new Date(pensamento.data)
+      }
     }
     catch {
       alert('Erro ao buscar pensamento')
@@ -51,6 +73,32 @@ const api = {
     }
     catch {
       alert('Erro ao excluir um pensamento')
+      throw error
+    }
+  }, 
+
+  async buscarPensamentosPorTermo(termo) {
+    try {
+      const pensamentos = await this.buscarPensamentos()
+      const termoEmMinusculas = termo.toLowerCase()
+
+      const pensamentosFiltrados = pensamentos.filter(pensamento => {
+        return pensamento.conteudo.toLowerCase().includes(termoEmMinusculas) ||
+        pensamento.autoria.toLowerCase().includes(termoEmMinusculas)
+      })
+      return pensamentosFiltrados
+    } catch (error) {
+      alert("Erro ao filtrar pensamentos")
+      throw error
+    }
+  },
+  
+  async atualizarFavorito(id, favorito) {
+    try {
+      const response = await axios.patch(`${URL_BASE}/pensamentos/${id}`, { favorito })
+      return response.data
+    } catch (error) {
+      alert("Erro ao atualizar favorito")
       throw error
     }
   }
